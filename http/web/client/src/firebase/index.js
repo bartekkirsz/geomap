86ee257ac6c2d4c6b9f9a8c49e58b86645666d23
Bartek Kirsz <bartek.kirsz@gmail.com>
2020-06-29 22:11:18 +0200

    Next commit

--- http/web/client/src/firebase/index.js
+++ http/web/client/src/firebase/index.js
@@ -2,16 +2,18 @@ import firebase from 'firebase/app';
 import 'firebase/storage';
 
 // Initialize Firebase
+
 var config = {
-  apiKey: "AIzaSyA5mnKTiz-T7swjmDCB7tlFpMGhgOUWFxE",
-    authDomain: "geomap-deb38.firebaseapp.com",
-    databaseURL: "https://geomap-deb38.firebaseio.com",
-    projectId: "geomap-deb38",
-    storageBucket: "geomap-deb38.appspot.com",
-    messagingSenderId: "531595922585",
-    appId: "1:531595922585:web:14b4408aaeea80882a7c08",
-    measurementId: "G-4ZJBB01669"
+  apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
 };
+
 firebase.initializeApp(config);
 
 const storage = firebase.storage();
